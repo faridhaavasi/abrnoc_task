770840813d603078e0a0e859c52549fec66ba604
faridhaavasi <faridhavasi777@gmail.com>
2025-01-07 13:44:08 +0330

    Add JWT token authentication endpoints to the authentication app

--- apps/authentication/v1/urls.py
+++ apps/authentication/v1/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 
 from apps.authentication.v1.views.users import RegisterUserView
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 urlpatterns = [
     path('register/', RegisterUserView.as_view(), name='register_user'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
